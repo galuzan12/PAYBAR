ccc67a351f7f5e678a1122d4885b10f47d8d88fb
lipazd <lipazdahan@gmail.com>
2020-08-26 13:31:12 +0300

    try

---
+++ client_side/src/Components/Pages/CheckoutPage/Checkout.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Checkout.css';
+import { Container } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div>
+      asa
+    </div>
+  );
+}
+
+export default App;
