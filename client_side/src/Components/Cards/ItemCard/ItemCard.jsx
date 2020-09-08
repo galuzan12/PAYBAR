01edaf70b2c4022c285891029038984bd9a0f0d5
lipazd <lipazdahan@gmail.com>
2020-09-08 17:50:24 +0300

    more styling

--- client_side/src/Components/Cards/ItemCard/ItemCard.jsx
+++ client_side/src/Components/Cards/ItemCard/ItemCard.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import '../../../App.css';
 import './ItemCard.css';
 import { Row, Col } from 'react-bootstrap';
-import { AiFillStar } from 'react-icons/ai'
+import { GiQueenCrown } from 'react-icons/gi'
 function ItemCard(props) {
     const { productName, productPrice, productDescription, importantNotes, isPopular } = props.item;
 
     const addProductToCart = () => {
         alert("bbb")
     }
+
+    // בינתייםם
+    const countItem = 2;
     return (
         <Row onClick={addProductToCart} className="itemcard_row">
             <div className="itemcard_img_col">
@@ -15,23 +19,32 @@ function ItemCard(props) {
             </div>
             <div className="itemcard_text">
                 <div className="productName"> {productName} </div>
-                <div  className="productPrice"> {productPrice} ₪</div>
-                <div  className="productDescription"> {productDescription} </div>
-                    <div className="itemcard_text_row">
-                        <div className="importantNotes">
-                            {
-                                importantNotes.map((item, key) =>
-                                    key + 1 != importantNotes.length ? item + "," : item
-                                )}
-                        </div>
-
+                <div className="productPrice"> {productPrice} ₪</div>
+                <div className="productDescription"> {productDescription} </div>
+                <div className="itemcard_text_row">
+                    <div className="importantNotes">
+                        {
+                            importantNotes.map((item, key) =>
+                                key + 1 != importantNotes.length ? item + "," : item
+                            )}
                     </div>
 
-                    {isPopular && <Row  className="itemcard_text_row">
-                        <div className="POPULAR"> POPULAR <AiFillStar color="#FFD700" />  </div>
+                </div>
 
-                    </Row>}
-                
+                {isPopular && <Row className="itemcard_text_row">
+                    <div className="POPULAR"> POPULAR <GiQueenCrown color="#FFD700" />  </div>
+
+                </Row>}
+
+                <Row className="addToCart">
+                    <div class="addToCart_text">הוסף לעגלה</div>  
+                    <Row>
+                    <button class="plus">+</button>
+                    <div class="CountItem">{countItem}</div>
+                    <button class="minus">-</button>
+                    </Row>
+                </Row>
+
             </div>
         </Row>
     );
